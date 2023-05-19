4e46beda22ab34dc6aec7a7c1e59b8085245ba90
Álvaru <78793330+ch0pex@users.noreply.github.com>
2023-05-19 03:24:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -60,39 +60,3 @@ if __name__ == "__main__":
     
 
     print(time.time() - start)
-
-
-       
-    
-
-
-"""
-        film = {
-            "Title": imdb.select("title")[0].text[:-25].split(" (")[0],
-            "year": imdb.select("title")[0].text[:-25].split(" (")[1],
-            ****HAY MAS STAR RATING****"score": imdb.find("span", {"class" :"ipl-rating-star__rating"}).text,
-            "genre": get_storyline,
-            "sumary": get_storyline,
-            "country": get_storyline,
-            "runtime": get_storyline,
-            "language": get_storyline,         
-            "poster": imdb.find("img", {"alt" :"Poster"}).get_attribute_list("src")[0]
-            "director": crew = imdb.findAll("div", {"class":"titlereference-overview-section"})
-                        for element in crew: 
-                            if "Director" in element.text:cle
-                                aux_list = element.findAll("a")
-                                directors = [z.text for z in aux_list]
-                                break,
-            "writers" : lista = imdb.find("table", {"class" :"simpleTable spFirst writers_list"}).findAll("td", {"class":"name"})
-                        writters = [z.text.strip() for z in lista]
-            "actors" : lista = imdb.find("table", {"class" :"cast_list"}).findAll("span", {"itemprop":"name"})
-                        actors = [z.text for z in lista]
-        }
-        
-        print(film)
-        
-        
-        
-        
-        
-"""
